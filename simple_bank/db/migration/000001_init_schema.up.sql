28a14842d67b0be2caad73e9e3a21d18a3988092
Bakare Damilola <bdamilola00@gmail.com>
2024-02-18 02:53:06 +0100

    fix: change balance datatype to int

--- simple_bank/db/migration/000001_init_schema.up.sql
+++ simple_bank/db/migration/000001_init_schema.up.sql
@@ -1,14 +1,14 @@
 CREATE TABLE "accounts" (
   "id" bigserial PRIMARY KEY,
   "owner" varchar NOT NULL,
-  "balance" decimal NOT NULL,
+  "balance" bigint NOT NULL,
   "currency" varchar NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "entries" (
   "id" bigserial PRIMARY KEY,
-  "amount" decimal NOT NULL,
+  "amount" bigint NOT NULL,
   "account_id" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
@@ -17,7 +17,7 @@ CREATE TABLE "transfers" (
   "id" bigserial PRIMARY KEY,
   "from_account_id" bigint NOT NULL,
   "to_account_id" bigint NOT NULL,
-  "amount" decimal NOT NULL,
+  "amount" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 

43355106709434693a2319b387ee83f4d5181229
earnstein <bdamilola00@gmail.com>
2024-04-06 22:44:54 +0100

    chore: change postgres  port to 5432

--- simple_bank/db/sqlc/main_test.go
+++ simple_bank/db/sqlc/main_test.go
@@ -9,18 +9,17 @@ import (
 	_ "github.com/lib/pq"
 )
 
-
 const (
 	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5433/simple_bank?sslmode=disable"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
 
-func TestMain(m *testing.M){
+func TestMain(m *testing.M) {
 	conn, err := sql.Open(dbDriver, dbSource)
 	if err != nil {
-		log.Fatal("Cannot connect to db:", err)
+		log.Fatal("cannot connect to db:", err)
 	}
 
 	testQueries = New(conn)

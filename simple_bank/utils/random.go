92acaa7c3ec7a1431706a75687c8369896e21cc0
earnstein <bdamilola00@gmail.com>
2024-04-06 22:46:15 +0100

    chore : refatored Random string function

--- simple_bank/utils/random.go
+++ simple_bank/utils/random.go
@@ -6,26 +6,35 @@ import (
 	"time"
 )
 
-const alphabets ="abcdefghijklmnopqrstuvwxyz"
-
-func init()  {
+func init() {
 	rand.NewSource(time.Now().UnixNano())
 }
 
 // RandomInt generate a random number between min , max
-func RandomInt(min, max int64) int64  {
-	return min + rand.Int63n(max - min + 1) // returns value between (0 , max-min+1)
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1) // returns value between (0 , max-min+1)
 }
 
-// RandomString generates a random string of length n
-func RandomString(n int) string  {
-	var sb strings.Builder
-	k := len(alphabets)
-
-	for i := 0; i < n; i++{
-		c := alphabets[rand.Intn(k)]
-		sb.WriteByte(c)
+// GenerateRandomString generates a random string of length n
+func GenerateRandomString(length int) string {
+	const chars = "abcdefghijklmnopqrstuvwxyz"
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = chars[rand.Intn(len(chars))]
 	}
+	return string(b)
+}
 
-	return sb.String()
+func RandomOwner() string {
+	return strings.ToLower(GenerateRandomString(6))
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "CAD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
 }

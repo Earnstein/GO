690dd016faa2ca2e3756edf24f93767e17bfd214
earnstein <bdamilola00@gmail.com>
2024-04-24 13:19:49 +0100

    add: event, login and check origin handler

--- webrtc/manager.go
+++ webrtc/manager.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -11,20 +16,59 @@ import (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin:     checkOrigin,
 }
 
 type Manager struct {
 	clients ClientList
 	sync.RWMutex
+	otps     RentensionMap
+	handlers map[string]EventHandler
 }
 
-func NewManager() *Manager {
-	return &Manager{
-		clients: make(ClientList),
+func NewManager(ctx context.Context) *Manager {
+	m := &Manager{
+		clients:  make(ClientList),
+		handlers: make(map[string]EventHandler),
+		otps:     NewRentionMap(ctx, 5*time.Second),
 	}
+	m.setupEventHandlers()
+	return m
 }
 
+func (m *Manager) setupEventHandlers() {
+	m.handlers[EventSendMessage] = SendMessage
+}
+
+func SendMessage(event Event, client *Client) error {
+	fmt.Println(event)
+	log.Println("Message sent")
+	return nil
+}
+
+func (manager *Manager) routeEvent(event Event, client *Client) error {
+	if handler, ok := manager.handlers[event.Type]; ok {
+		if err := handler(event, client); err != nil {
+			return err
+		}
+		return nil
+	} else {
+		return errors.New("event not found")
+	}
+
+}
 func (manager *Manager) ServeWS(w http.ResponseWriter, r *http.Request) {
+
+	otp := r.URL.Query().Get("otp")
+	if otp == "" {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	if !manager.otps.verifyOTP(otp) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
 	log.Println("New connection")
 	// upgrade regular http connection into web socket
 
@@ -42,20 +86,58 @@ func (manager *Manager) ServeWS(w http.ResponseWriter, r *http.Request) {
 	go client.WriteMessages()
 }
 
+func (m *Manager) LoginHandler(w http.ResponseWriter, r *http.Request) {
+	type userLoginRequest struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
 
+	var req userLoginRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if req.Username == "admin" && req.Password == "admin" {
+		type response struct {
+			OTP string `json:"otp"`
+		}
+		otp := m.otps.NewOTP()
+		resp := response{
+			OTP: otp.Key,
+		}
+
+		data, err := json.Marshal(resp)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write(data)
+	}
+}
 func (m *Manager) addClient(client *Client) {
 	m.Lock()
 	defer m.Unlock()
 	m.clients[client] = true
 }
 
-
 func (m *Manager) removeClient(client *Client) {
 	m.Lock()
 	defer m.Unlock()
-	
+
 	if _, ok := m.clients[client]; ok {
 		client.connection.Close()
 		delete(m.clients, client)
+	}
+}
+
+func checkOrigin(r *http.Request) bool {
+	origin := r.Header.Get("Origin")
+	switch origin {
+	case "http://localhost:8080":
+		return true
+	default:
+		return false
 	}
 }

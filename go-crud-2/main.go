7f9adcb20a9a1737d437b3b0ecb0badf7176275b
earnstein <bdamilola00@gmail.com>
2024-04-28 00:58:48 +0100

    refactor file

--- go-crud-2/main.go
+++ go-crud-2/main.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"example.com/earnstein-api/controller"
 	"example.com/earnstein-api/services"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -21,13 +23,15 @@ var (
 	usercollection *mongo.Collection
 	mongoclient    *mongo.Client
 	err            error
+	MONGO_URL		string
 )
 
 func init() {
+	godotenv.Load()
 	ctx := context.TODO()
-
+	MONGO_URL = os.Getenv("MONGO_URL")
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	mongoconn := options.Client().ApplyURI("mongodb+srv://earnstein:Einstein5914@earnstein-api.in5mcyj.mongodb.net/?retryWrites=true&w=majority&appName=earnstein-api").SetServerAPIOptions(serverAPI)
+	mongoconn := options.Client().ApplyURI(MONGO_URL).SetServerAPIOptions(serverAPI)
 	mongoclient, err = mongo.Connect(ctx, mongoconn)
 	if err != nil {
 		log.Fatal(err)

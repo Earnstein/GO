103a06c332e19d41680e93eabd0d1bfbf5ffe8f8
earnstein <bdamilola00@gmail.com>
2024-05-09 23:42:47 +0100

    feat: added database setup

--- snippetsAPI/cmd/web/main.go
+++ snippetsAPI/cmd/web/main.go
@@ -1,34 +1,73 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/earnstein/GO/snippetsAPI/cmd/controllers"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
+type Application struct {
+	InfoLog  *log.Logger
+	ErrorLog *log.Logger
+}
 
+func NewApplicaton(infoLogger, errorLogger *log.Logger) *Application {
+	return &Application{
+		InfoLog:  infoLogger,
+		ErrorLog: errorLogger,
+	}
+}
+
+func NewServer(addr string, logger *log.Logger, app *Application) *http.Server {
+	return &http.Server{
+		Addr:     addr,
+		ErrorLog: logger,
+		Handler:  app.routes(),
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+	return nil, err
+	}
+	if err = db.Ping(); err != nil {
+	return nil, err
+	}
+	return db, nil
+	}
 
 func main() {
-	// command flags
-	addr := flag.String("addr", ":5000", "HTTP network port address")
-	flag.Parse()
-
 	// loggers
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
-	app := controllers.NewApplicaton(infoLog, errorLog)
+
+	 if err := godotenv.Load(); err != nil {
+		errorLog.Fatal(err)
+	 }
+
+	// command flags
+	addr := flag.String("addr", os.Getenv("ADDR"), "HTTP network port address")
+	dsn := flag.String("dsn", os.Getenv("DB_CONN_STRING"), "MYSQL data source name")
+	flag.Parse()
+
+	// database configuration
+	db , err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	// server
-	server := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	server.Handle("/static/", http.StripPrefix("/static", fileServer))
-	server.HandleFunc("/", app.HomeHandler)
-	server.HandleFunc("/create", app.HandleSnippetCreate)
-	server.HandleFunc("/snippet/view", app.HandleSnippetView)
+	app := NewApplicaton(infoLog, errorLog)
+	server := NewServer(*addr, errorLog, app)
 	infoLog.Printf("server is listening on port %s", *addr)
-	err := http.ListenAndServe(*addr, server)
+	err = server.ListenAndServe()
 	errorLog.Fatal(err)
 }

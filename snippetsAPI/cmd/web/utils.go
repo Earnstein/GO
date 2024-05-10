de59ae9c02a8d186868a25dd66630821a1fcefa6
earnstein <bdamilola00@gmail.com>
2024-05-10 23:36:39 +0100

    add: function to return error as json

--- snippetsAPI/cmd/web/utils.go
+++ snippetsAPI/cmd/web/utils.go
@@ -1,23 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"runtime/debug"
 )
 
+type ErrorMessge map[string]string
+
+func NewJsonResponse( app *Application, w http.ResponseWriter, statusCode int) []byte {
+	newJsonmessage := ErrorMessge{"error": http.StatusText(statusCode)}
+	errMsg, err := json.Marshal(newJsonmessage)
+	if err != nil {
+		app.serverError(w, err)
+		return nil
+	}
+	return errMsg
+}
+
 func (app *Application) serverError(w http.ResponseWriter,  err error){
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
 	app.ErrorLog.Output(2, trace)
-	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	errMsg := NewJsonResponse(app, w, http.StatusInternalServerError)
+	http.Error(w, string(errMsg), http.StatusInternalServerError)
 }
 
 
 func (app *Application) clientError(w http.ResponseWriter, statusCode int) {
-	http.Error(w, http.StatusText(statusCode), statusCode)
+	errMsg := NewJsonResponse(app, w, statusCode)
+	http.Error(w, string(errMsg), statusCode)
 }
 
-func (app *Application) NotFound(w http.ResponseWriter){
+func (app *Application) notFound(w http.ResponseWriter){
 	app.clientError(w, http.StatusNotFound)
 }
 

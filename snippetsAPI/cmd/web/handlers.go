d79a9a6064c709ed1bb572ca24fb82d8e410f74e
earnstein <bdamilola00@gmail.com>
2024-05-10 18:25:11 +0100

    add: create snippet handler

--- snippetsAPI/cmd/web/handlers.go
+++ snippetsAPI/cmd/web/handlers.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
 	"strconv"
+
+	"github.com/earnstein/GO/snippetsAPI/internal/models"
 )
 
-
-
-func (app *Application) HomeHandler(w http.ResponseWriter, r *http.Request) {
+func (app *Application) homeHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		app.NotFound(w)
 		return
@@ -31,17 +32,37 @@ func (app *Application) HomeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 
-func(app *Application) HandleSnippetCreate(w http.ResponseWriter, r *http.Request) {
+func(app *Application) handleSnippetCreate(w http.ResponseWriter, r *http.Request) {
+	var reqBody models.SnippetBody
+	err := json.NewDecoder(r.Body).Decode(&reqBody)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
 	if r.Method != http.MethodPost {
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Allow", http.MethodPost)
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	w.Write([]byte("OK"))
+	
+	id , err := app.snippets.Insert(reqBody.Title, reqBody.Content, reqBody.Expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	jsonResponse := map[string]string{"message": "movie snippets created successfully", "id": fmt.Sprintf("%v", id), "title": reqBody.Title}
+	msg, err := json.Marshal(jsonResponse)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	
+	fmt.Fprintln(w, string(msg))
 }
 
-func(app *Application) HandleSnippetView(w http.ResponseWriter, r *http.Request) {
+func(app *Application) handleSnippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		app.NotFound(w)

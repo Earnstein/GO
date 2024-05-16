56766f5d124ff00480e24df878c91458e9d4c921
earnstein <bdamilola00@gmail.com>
2024-05-16 01:22:19 +0100

    add: validation on user request

--- snippetsAPI/internal/models/users.go
+++ snippetsAPI/internal/models/users.go
@@ -2,7 +2,12 @@ package models
 
 import (
 	"database/sql"
+	"errors"
+	"strings"
 	"time"
+
+	"github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -14,9 +19,9 @@ type User struct {
 }
 
 type UserRequestBody struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Name     string `json:"name" validate:"required" binding:"required"`
+	Email    string `json:"email" validate:"required" binding:"required"`
+	Password string `json:"password" validate:"required" binding:"required"`
 }
 
 type UserModel struct {
@@ -24,6 +29,26 @@ type UserModel struct {
 }
 
 func (m *UserModel) Insert(name, email, password string) error {
+	hashed_password, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	stmt := `INSERT INTO users (name, email, hashed_password, created)
+		VALUES
+		(?, ?, ?, UTC_TIMESTAMP())
+		`
+	_, err = m.DB.Exec(stmt, name, email, string(hashed_password))
+
+	if err != nil {
+		var queryErr *mysql.MySQLError
+		if errors.As(err, &queryErr) {
+			if queryErr.Number == 1062 && strings.Contains(queryErr.Message, "users_uc_email") {
+				return ErrDuplicateEmail
+			}
+		}
+		return err
+	}
 	return nil
 }
 

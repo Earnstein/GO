1c974fd64e365aa4de19e7f10ccdca17429b7c64
earnstein <bdamilola00@gmail.com>
2024-05-16 01:20:59 +0100

    add: user routes

--- snippetsAPI/cmd/web/routes.go
+++ snippetsAPI/cmd/web/routes.go
@@ -18,10 +18,18 @@ func (app *Application) routes() http.Handler {
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
 	sessionMiddleware := alice.New(app.sessionManager.LoadAndSave)
+
+	// snippets routes
 	router.Handler(http.MethodGet, "/", sessionMiddleware.ThenFunc(app.homeHandler))
 	router.Handler(http.MethodPost, "/snippet/create", sessionMiddleware.ThenFunc(app.handleSnippetCreate))
 	router.Handler(http.MethodGet, "/snippet/view/:id", sessionMiddleware.ThenFunc(app.handleSnippetView))
 	router.Handler(http.MethodGet, "/snippet/latest", sessionMiddleware.ThenFunc(app.handleSnippetList))
+
+	// user routes
+
+	router.Handler(http.MethodPost, "/user/sign-up", sessionMiddleware.ThenFunc(app.handleUserSignup))
+	router.Handler(http.MethodPost, "/user/sign-in", sessionMiddleware.ThenFunc(app.handleUserSignin))
+	router.Handler(http.MethodGet, "/user/logout", sessionMiddleware.ThenFunc(app.handleUserLogout))
 
 	middlewareChain := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 

f7d7f39ec3a68f27db514310cef97f48cb7f2a78
earnstein <bdamilola00@gmail.com>
2024-05-18 17:03:42 +0100

    chore: refactored file and served html using the embed system

--- snippetsAPI/cmd/web/routes.go
+++ snippetsAPI/cmd/web/routes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/earnstein/GO/snippetsAPI/ui"
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
@@ -14,24 +15,27 @@ func (app *Application) routes() http.Handler {
 		app.notFound(w)
 	})
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+	fileServer := http.FileServer(http.FS(ui.Files))
+	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 
-	sessionMiddleware := alice.New(app.sessionManager.LoadAndSave)
+	router.HandlerFunc(http.MethodGet, "/ping", ping)
 
-	// snippets routes
-
-	protectedMiddleware := sessionMiddleware.Append(app.requireAuthentication)
-	router.Handler(http.MethodGet, "/", protectedMiddleware.ThenFunc(app.homeHandler))
-	router.Handler(http.MethodPost, "/snippet/create", protectedMiddleware.ThenFunc(app.handleSnippetCreate))
-	router.Handler(http.MethodGet, "/snippet/view/:id", protectedMiddleware.ThenFunc(app.handleSnippetView))
-	router.Handler(http.MethodGet, "/snippet/latest", protectedMiddleware.ThenFunc(app.handleSnippetList))
+	sessionMiddleware := alice.New(app.sessionManager.LoadAndSave, app.authenticate)
 
 	// user routes
 
 	router.Handler(http.MethodPost, "/user/sign-up", sessionMiddleware.ThenFunc(app.handleUserSignup))
 	router.Handler(http.MethodPost, "/user/sign-in", sessionMiddleware.ThenFunc(app.handleUserSignin))
 	router.Handler(http.MethodGet, "/user/logout", sessionMiddleware.ThenFunc(app.handleUserLogout))
+
+	// snippets routes
+
+	protectedMiddleware := sessionMiddleware.Append(app.requireAuthentication)
+
+	router.Handler(http.MethodGet, "/", protectedMiddleware.ThenFunc(app.homeHandler))
+	router.Handler(http.MethodPost, "/snippet/create", protectedMiddleware.ThenFunc(app.handleSnippetCreate))
+	router.Handler(http.MethodGet, "/snippet/view/:id", protectedMiddleware.ThenFunc(app.handleSnippetView))
+	router.Handler(http.MethodGet, "/snippet/latest", protectedMiddleware.ThenFunc(app.handleSnippetList))
 
 	middlewareChain := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 

c09b5928eea8ff6337f44872a71e0054a4e2f423
earnstein <bdamilola00@gmail.com>
2024-05-18 10:58:20 +0100

    add: authentication middleware

--- snippetsAPI/cmd/web/middleware.go
+++ snippetsAPI/cmd/web/middleware.go
@@ -36,3 +36,14 @@ func (app *Application) recoverPanic(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (app *Application) requireAuthentication(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !app.isAuthenticated(r) {
+			app.clientError(w, http.StatusUnauthorized)
+			return
+		}		
+		w.Header().Add("Cache-Control", "no-store")
+		next.ServeHTTP(w, r)
+	})
+}

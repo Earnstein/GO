5199bf7a1745609f09e3fabda66069dc0b6813d7
earnstein <bdamilola00@gmail.com>
2024-05-21 20:07:10 +0100

    add: failed validation response handler

--- greenlight/cmd/api/errors.go
+++ greenlight/cmd/api/errors.go
@@ -38,3 +38,7 @@ func (app *application) methodNotAllowedHandler(w http.ResponseWriter, r *http.R
 func (app *application) badRequestErrorHandler(w http.ResponseWriter, r *http.Request, err error) {
 	app.errorHandler(w, r, http.StatusBadRequest, err.Error())
 }
+
+func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
+	app.errorHandler(w, r, http.StatusUnprocessableEntity, errors)
+}

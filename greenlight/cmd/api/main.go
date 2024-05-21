9ee00ed82cafeed0dc91511ff16f8b383aed3d3f
earnstein <bdamilola00@gmail.com>
2024-05-21 20:07:55 +0100

    add: setup postgres configuration

--- greenlight/cmd/api/main.go
+++ greenlight/cmd/api/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,6 +19,12 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db struct {
+		dsn string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
+	}
 }
 
 type application struct {
@@ -21,10 +32,10 @@ type application struct {
 	logger *log.Logger
 }
 
-func NewApplication(config config) *application {
+func NewApplication(config config, logger *log.Logger) *application {
 	return &application{
 		config: config,
-		logger: log.New(os.Stdout, "", log.Ldate|log.Ltime),
+		logger: logger,
 	}
 }
 
@@ -38,16 +49,59 @@ func NewServer(config config, app *application) *http.Server {
 	}
 }
 func main() {
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	if err := godotenv.Load(); err != nil {
+		logger.Fatal(err);
+	}
+	
 	var cfg config
 
+	// Server flags
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	
+	// Database flags
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("DB_DSN"), "PostgreSQL DSN")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
 	flag.Parse()
 
-	app := NewApplication(cfg)
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
+
+	logger.Println("database connection pool is established")
+	app := NewApplication(cfg, logger)
 	srv := NewServer(cfg, app)
 
 	app.logger.Printf("Starting %s server on port %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	app.logger.Fatal(err)
+}
+
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+	db.SetConnMaxIdleTime(duration)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	
+	if err = db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }

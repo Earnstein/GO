3b5184f5f9cba2886fd1ad74d2d259d3da829fbc
earnstein <bdamilola00@gmail.com>
2024-05-22 19:01:52 +0100

    feat: added update/delete movie handler

--- greenlight/cmd/api/movies.go
+++ greenlight/cmd/api/movies.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/earnstein/GO/greenlight/internal/data"
 	"github.com/earnstein/GO/greenlight/internal/validator"
@@ -28,16 +28,21 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		Runtime: reqBody.Runtime,
 		Genres:  reqBody.Genres,
 	}
-
 	// Validation
 	v := validator.New()
 	if data.ValidateMovie(v, movie); !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
-	
-	data := envelope{"status": "ok", "movie": reqBody}
-	app.writeJSONResponse(w, http.StatusCreated, data, nil)
+	err := app.models.Movies.Insert(movie)
+	if err != nil {
+		app.serverErrorHandler(w, r, err)
+	}
+	headers := make(http.Header)
+	headers.Set("Location", fmt.Sprintf("/v1/movies/%d", movie.ID))
+
+	response := envelope{"status": "ok", "movie": reqBody}
+	app.writeJSONResponse(w, http.StatusCreated, response, headers)
 }
 
 func (app *application) getMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -47,21 +52,100 @@ func (app *application) getMovieHandler(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
-	Movie := data.Movie{
-		ID:        id,
-		Title:     "Casablanca",
-		Year:      1942,
-		Runtime:   102,
-		CreatedAt: time.Now(),
-		Genres:    []string{"drama", "romance", "war"},
-		Version:   1,
+	movie, err := app.models.Movies.Get(id)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundHandler(w, r)
+		default:
+			app.serverErrorHandler(w, r, err)
+		}
+		return
 	}
-	data := envelope{"movie": Movie}
-	err = app.writeJSONResponse(w, http.StatusOK, data, nil)
+
+	response := envelope{"movie": movie}
+	err = app.writeJSONResponse(w, http.StatusOK, response, nil)
 	if err != nil {
 		app.serverErrorHandler(w, r, err)
 	}
 
+}
+
+func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIdParam(r)
+	if err != nil {
+		app.notFoundHandler(w, r)
+		return
+	}
+
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundHandler(w, r)
+		default:
+			app.serverErrorHandler(w, r, err)
+		}
+		return
+	}
+
+	var reqBody struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+
+	if err := app.readJSONResponse(w, r, &reqBody); err != nil {
+		app.badRequestErrorHandler(w, r, err)
+		return
+	}
+
+	movie.Title = reqBody.Title
+	movie.Year = reqBody.Year
+	movie.Runtime = reqBody.Runtime
+	movie.Genres = reqBody.Genres
+
+	v := validator.New()
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+	err = app.models.Movies.Update(movie)
+	if err != nil {
+		app.serverErrorHandler(w, r, err)
+	}
+
+	response := envelope{"movie": movie}
+	err = app.writeJSONResponse(w, http.StatusOK, response, nil)
+	if err != nil {
+		app.serverErrorHandler(w, r, err)
+	}
+
+}
+
+func (app *application) deleteMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIdParam(r)
+	if err != nil {
+		app.notFoundHandler(w, r)
+		return
+	}
+
+	if err = app.models.Movies.Delete(id); err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundHandler(w, r)
+		default:
+			app.serverErrorHandler(w, r, err)
+		}
+	}
+
+	response := envelope{"message": fmt.Sprintf("Movie with id %d is deleted successfully.", id)}
+	err = app.writeJSONResponse(w, http.StatusOK, response, nil)
+	if err != nil {
+		app.serverErrorHandler(w, r, err)
+	}
 }
 
 func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request) {

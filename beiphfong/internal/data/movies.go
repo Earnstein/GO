1e4c2e6c82961bba7944a1c5d49edc5e5fbae063
earnstein <bdamilola00@gmail.com>
2024-06-02 19:54:06 +0100

    chore: registered token model

--- beiphfong/internal/data/movies.go
+++ beiphfong/internal/data/movies.go
@@ -36,6 +36,7 @@ type (
 	Models struct {
 		Movies MovieModel
 		Users  UserModel
+		Tokens TokenModel
 	}
 )
 
@@ -43,6 +44,7 @@ func NewModels(db *sql.DB) *Models {
 	return &Models{
 		Movies: MovieModel{DB: db},
 		Users:  UserModel{DB: db},
+		Tokens: TokenModel{DB: db},
 	}
 }
 

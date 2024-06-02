ea16bb3242b16a6e46d87f1a5e4c5d8a151b644d
earnstein <bdamilola00@gmail.com>
2024-06-02 01:23:43 +0100

    chore: refactor to check for user by email

--- beiphfong/cmd/api/user.go
+++ beiphfong/cmd/api/user.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"net/http"
 
 	"github.com/earnstein/GO/greenlight/internal/data"
@@ -34,23 +33,24 @@ func (app *application) createUserHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	err = app.models.Users.Insert(newUser)
-
-	if err != nil {
-		switch {
-		case errors.Is(err, data.ErrDuplicateEmail):
-			v.AddError("email", "a user with this email address already exists")
-			app.failedValidationResponse(w, r, v.Errors)
-		default:
-			app.serverErrorHandler(w, r, err)
-		}
+	_, err = app.models.Users.GetByEmail(newUser.Email)
+	if err == nil {
+		v.AddError("email", "a user with this email address already exists")
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
-	response := envelope{"user": newUser}
-	err = app.writeJSONResponse(w, http.StatusCreated, response, nil)
 
+	err = app.models.Users.Insert(newUser)
 	if err != nil {
 		app.serverErrorHandler(w, r, err)
+		return
+	}
+
+	response := envelope{"user": newUser}
+	err = app.writeJSONResponse(w, http.StatusCreated, response, nil)
+	if err != nil {
+		app.serverErrorHandler(w, r, err)
+		return
 	}
 }
 

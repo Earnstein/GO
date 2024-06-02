45cd91bc1fb8b8c037f06a049bfb43d645f85975
earnstein <bdamilola00@gmail.com>
2024-06-02 20:06:07 +0100

    feat: token manager file

---
+++ beiphfong/internal/data/tokens.go
@@ -0,0 +1,91 @@
+package data
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/sha256"
+	"database/sql"
+	"encoding/base64"
+	"time"
+
+	"github.com/earnstein/GO/greenlight/internal/validator"
+)
+
+const (
+	ScopeActivation     = "activation"
+	ScopeAuthentication = "authentication"
+	ScopePasswordReset  = "password-reset"
+)
+
+type Token struct {
+	Plaintext string
+	Hash      []byte
+	UserId    int64
+	Expiry    time.Time
+	Scope     string
+}
+
+func generateToken(userID int64, ttl time.Duration, scope string) (*Token, error) {
+	token := &Token{
+		UserId: userID,
+		Expiry: time.Now().Add(ttl),
+		Scope:  scope,
+	}
+	randomBytes := make([]byte, 16)
+
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	token.Plaintext = base64.StdEncoding.WithPadding(base64.NoPadding).EncodeToString(randomBytes)
+	hash := sha256.Sum256([]byte(token.Plaintext))
+	token.Hash = hash[:]
+
+	return token, nil
+
+}
+
+func ValidateTokenPlaintext(v *validator.Validator, tokenPlaintext string) {
+	v.Check(tokenPlaintext != "", "token", "token must be provided")
+	v.Check(len(tokenPlaintext) == 26, "token", "token must be 26 bytes long")
+}
+
+type TokenModel struct {
+	DB *sql.DB
+}
+
+func (t *TokenModel) New(userID int64, ttl time.Duration, scope string) (*Token, error) {
+	token, err := generateToken(userID, ttl, scope)
+	if err != nil {
+		return nil, err
+	}
+	err = t.Insert(token)
+	return token, err
+}
+
+func (t *TokenModel) Insert(token *Token) error {
+	stmt := `
+		INSERT INTO tokens (hash, user_id, expiry, scope)
+		VALUES ($1, $2, $3, $4)
+	`
+	args := []any{token.Hash, token.UserId, token.Expiry, token.Scope}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := t.DB.ExecContext(ctx, stmt, args...)
+	return err
+}
+
+func (t *TokenModel) DeleteAllForUser(scope string, userID int64) error {
+	stmt := `
+		DELETE FROM tokens
+		WHERE scope = $1 AND user_id = $2
+	`
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := t.DB.ExecContext(ctx, stmt, scope, userID)
+	return err
+}

48015c1709757d6e85654e69add56c12f4279eb7
earnstein <bdamilola00@gmail.com>
2024-06-02 01:24:23 +0100

    add: mailer set up file and methods

---
+++ beiphfong/internal/mailer/mailer.go
@@ -0,0 +1,66 @@
+package mailer
+
+import (
+	"bytes"
+	"embed"
+	"html/template"
+	"time"
+
+	"github.com/go-mail/mail/v2"
+)
+
+var templateFS embed.FS
+
+type Mailer struct {
+	dialer *mail.Dialer
+	sender string
+}
+
+func New(host string, port int, username, password, sender string) Mailer {
+	dailer := mail.NewDialer(host, port, username, password)
+	dailer.Timeout = 5 * time.Second
+
+	mailer := Mailer{
+		dialer: dailer,
+		sender: sender,
+	}
+	return mailer
+}
+
+func (m Mailer) Send(recipient, tmpl string, data any) error {
+	t, err := template.New("email").ParseFS(templateFS, "templates/"+tmpl)
+	if err != nil {
+		return err
+	}
+
+	subject := new(bytes.Buffer)
+	err = t.ExecuteTemplate(subject, "subject", data)
+	if err != nil {
+		return err
+	}
+
+	plainBody := new(bytes.Buffer)
+	err = t.ExecuteTemplate(plainBody, "plainBody", data)
+	if err != nil {
+		return err
+	}
+
+	htmlBody := new(bytes.Buffer)
+	err = t.ExecuteTemplate(htmlBody, "htmlBody", data)
+	if err != nil {
+		return err
+	}
+
+	msg := mail.NewMessage()
+	msg.SetHeader("To", recipient)
+	msg.SetHeader("From", m.sender)
+	msg.SetHeader("Subject", subject.String())
+	msg.SetBody("text/plain", plainBody.String())
+	msg.AddAlternative("text/html", htmlBody.String())
+
+	err = m.dialer.DialAndSend(msg)
+	if err != nil {
+		return err
+	}
+	return nil
+}

4aea8bbf2cdba762e670f0f34829f800252d1635
earnstein <bdamilola00@gmail.com>
2024-06-02 15:52:36 +0100

    add: mailer struct, config/ sync for go routines

--- beiphfong/cmd/api/main.go
+++ beiphfong/cmd/api/main.go
@@ -8,10 +8,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/earnstein/GO/greenlight/internal/data"
 	"github.com/earnstein/GO/greenlight/internal/jsonlog"
+	"github.com/earnstein/GO/greenlight/internal/mailer"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
@@ -33,19 +35,30 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *jsonlog.Logger
 	models *data.Models
+	mailer mailer.Mailer
+	wg     sync.WaitGroup
 }
 
-func newApplication(config config, logger *jsonlog.Logger, models *data.Models) *application {
+func newApplication(config config, logger *jsonlog.Logger, models *data.Models, mailer mailer.Mailer) *application {
 	return &application{
 		config: config,
 		logger: logger,
 		models: models,
+		mailer: mailer,
 	}
 }
 
@@ -82,6 +95,12 @@ func main() {
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 
+	// Mail config flags
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 2525, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "14240df435c2d7", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "5d1ff73bd0c4b0", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Beiphfong <no-reply@beiphfong.earnstein.net>", "SMTP sender")
 	flag.Parse()
 
 	db, err := openDB(cfg)
@@ -92,9 +111,13 @@ func main() {
 
 	logger.PrintInfo("database connection pool is established", nil)
 
-	app := newApplication(cfg, logger, data.NewModels(db))
+	mailer := mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender)
+	app := newApplication(cfg, logger, data.NewModels(db), mailer)
 	err = app.serve()
-	logger.PrintFatal(err, nil)
+
+	if err != nil {
+		logger.PrintFatal(err, nil)
+	}
 }
 
 func openDB(cfg config) (*sql.DB, error) {

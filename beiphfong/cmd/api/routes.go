592e29a37b62cab16a1da99d1afdf2b0b64a8746
earnstein <bdamilola00@gmail.com>
2024-06-05 02:06:29 +0100

    chore: added activated / users endpoint.

--- beiphfong/cmd/api/routes.go
+++ beiphfong/cmd/api/routes.go
@@ -16,10 +16,13 @@ func (app *application) routes() http.Handler {
 
 	// SYSTEM CHECK HANDLER
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+
 	// USER ROUTES
 	router.HandlerFunc(http.MethodPost, "/v1/user", app.createUserHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/user/:id", app.getUserHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/user/:id", app.updateUserHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/user/activated", app.activeUserHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/users", app.getAllUsers)
 
 	// MOVIES ROUTES
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)

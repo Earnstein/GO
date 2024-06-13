293c0b79ae2ed9ae92ab521f875927e77f09ca95
earnstein <bdamilola00@gmail.com>
2024-06-13 16:48:37 +0100

    add: permission file

---
+++ beiphfong/internal/data/permissions.go
@@ -0,0 +1,55 @@
+package data
+
+import (
+	"context"
+	"database/sql"
+	"time"
+)
+
+type Permissions []string
+
+func (p Permissions) Include(code string) bool {
+	for i := range p {
+		if code == p[i] {
+			return true
+		}
+	}
+	return false
+}
+
+type PermissionModel struct {
+	DB *sql.DB
+}
+
+func (pm *PermissionModel) GetAllUserPermission(userId int64) (Permissions, error) {
+	stmt := `SELECT permissions.code 
+		FROM permissions
+		INNER JOIN users_permissions ON users_permissions.permission_id = permission_id
+		INNER JOIN users ON users_permissions.user_id = users.id
+		WHERE users.id = $1`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var permissions Permissions
+
+	rows, err := pm.DB.QueryContext(ctx, stmt, userId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var permission string
+		err := rows.Scan(&permission)
+		if err != nil {
+			return nil, err
+		}
+		permissions = append(permissions, permission)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return permissions, nil
+}
